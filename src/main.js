a135be7f8986e9ac2e70b9946a303f3e84f19fd5
frostzzone <65735427+frostzzone@users.noreply.github.com>
2021-11-24 19:03:28 +0000

    Really added webserver

--- src/main.js
+++ src/main.js
@@ -177,9 +177,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200);
   res.end('Bot is online');
 });
-server.listen(3000); 
-
-            `;
+server.listen(3000);`;
         }
     }
 });
