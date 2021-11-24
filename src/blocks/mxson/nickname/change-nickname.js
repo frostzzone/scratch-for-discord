4886535e19c6d07b0c78ef5571f4fa06c08809a7
baconitious <80984948+baconitious@users.noreply.github.com>
2021-11-24 19:16:34 +0000

    Update change-nickname.js

--- src/blocks/mxson/nickname/change-nickname.js
+++ src/blocks/mxson/nickname/change-nickname.js
@@ -34,6 +34,7 @@ Blockly.Blocks[blockName] = {
 Blockly.JavaScript[blockName] = function(block){
     const member = Blockly.JavaScript.statementToCode(block, "MEMBER");
     const string = Blockly.JavaScript.statementToCode(block, "STRING");
+    const statements = Blockly.JavaScript.statementToCode(block, "BLOCK");
      const code = `${member}.setNickname({nick:${string}}) => {\n${statements}\n};\n`;
     return code;
 };
