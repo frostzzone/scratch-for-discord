5340c497e5f7f8cde5cd4fdcc434c499f1c7fc7b
frostzzone <65735427+frostzzone@users.noreply.github.com>
2021-11-26 02:02:06 -0800

    Organize

--- src/toolbox.js
+++ src/toolbox.js
@@ -1077,23 +1077,24 @@ export default (Blockly, value) => {
                     </shadow>
                 </value>
             </block>
-            <label text="%{BKY_BASE_BLOCKS}"></label>
-<block type="frost_env">
-<value name="VALUE">
-                    <shadow type="text">
-                        <field name="TEXT">token</field>
-                    </shadow>
-                </value>
-            </block>
-            <block type="s4d_on_connected"></block>
-            <label text="%{BKY_BOT_INFORMATIONS}"></label>
-<block type="frost_webserver">
+	    <label text="Repl"></label>
+	    <block type="frost_webserver">
 <value name="URL">
                     <shadow type="text">
                         <field name="TEXT">This site was created to keep bot on 25/8</field>
                     </shadow>
                 </value>
 </block>
+	    <block type="frost_env">
+<value name="VALUE">
+                    <shadow type="text">
+                        <field name="TEXT">token</field>
+                    </shadow>
+                </value>
+            </block>
+            <label text="%{BKY_BASE_BLOCKS}"></label>
+            <block type="s4d_on_connected"></block>
+            <label text="%{BKY_BOT_INFORMATIONS}"></label>
             <block type="s4d_bot_ping"></block>
             <block type="s4d_bot_server_count"></block>
             <block type="s4d_get_bot"></block>
