7516cc271cb28499d9667453b60435b0f36174c7
AlexCdDg2 <105122938+AlexCdDg2@users.noreply.github.com>
2022-08-23 21:46:01 -0300

    #mutators_everywhere

--- src/blocks/Eruption/message_embed/embed_set_author.js
+++ src/blocks/Eruption/message_embed/embed_set_author.js
@@ -1,47 +1,118 @@
 import * as Blockly from "blockly/core";
+import BaseBlockly from "blockly";
+
 import { registerRestrictions } from "../../../restrictions";
 
+
 const blockName = "s4d_embed_set_author";
+  const BORDER_FIELDS = ["AUTHOR", "PROFILE", "URL"];
+const BORDER_TYPES = ["","",""];
+const names = ["embed author", "set profile", "add url"];
+const amountOfInputs = names.length
+const menuName = 'idk'
+
 
 const blockData = {
-    "message0": "set embed author %1 set profile %2 add url (optional) %3",
-    "args0": [
-      {
-        "type": "input_value",
-        "name": "AUTHOR"
-      },
-      {
-        "type": "input_value",
-        "name": "PROFILE"
-      },
-      {
-        "type": "input_value",
-        "name": "URL"
-      }
-    ],
+    "message0": "set",
+    "colour": "#40BF4A",
     "previousStatement": null,
     "nextStatement": null,
-    "colour": "#40BF4A",
     "tooltip": "Set the author of an embed.",
     "helpUrl": ""
 };
 
-Blockly.Blocks[blockName] = {
-    init: function() {
-        this.jsonInit(blockData);
+
+Blockly.Blocks[menuName] = {
+    init: function () {
+        this.setColour("40BF4A");
+        this.setHelpUrl("");
     }
 };
+Blockly.Blocks[blockName] = {
+    init: function () {
+        this.jsonInit(blockData);
+        this.setMutator(new Blockly.Mutator([]));
+        this.inputs_ = []
+        for (let i = 0; i < amountOfInputs; i++) {
+            this.inputs_.push(false)
+        }
+    },
 
-Blockly.JavaScript[blockName] = function(block){
-    const author = Blockly.JavaScript.valueToCode(block, "AUTHOR", Blockly.JavaScript.ORDER_ATOMIC);
-    const profile = Blockly.JavaScript.valueToCode(block, "PROFILE", Blockly.JavaScript.ORDER_ATOMIC);
-    const url = Blockly.JavaScript.valueToCode(block, "URL", Blockly.JavaScript.ORDER_ATOMIC);
-    if (url.length == 0) {
-      const code = `embed.setAuthor({name: String(${author}), iconURL: String(${profile})}); \n`;
-    return code;
+
+    mutationToDom: function () {
+        if (!this.inputs_) {
+            return null;
+        }
+        const container = document.createElement("mutation");
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if (this.inputs_[i]) container.setAttribute(BORDER_FIELDS[i], this.inputs_[i])
+        }
+        return container;
+    },
+
+    domToMutation: function (xmlElement) {
+        for (let i = 0; i < this.inputs_.length; i++) {
+            this.inputs_[i] = xmlElement.getAttribute(BORDER_FIELDS[i].toLowerCase()) == "true";
+        }
+        this.updateShape_();
+    },
+
+    decompose: function (workspace) {
+        const containerBlock = workspace.newBlock(menuName);
+        for (let i = 0; i < this.inputs_.length; i++) {
+            BaseBlockly.Msg[BORDER_FIELDS[i]] = names[i];
+            containerBlock.appendDummyInput()
+                .setAlign(Blockly.ALIGN_LEFT)
+                .appendField(new Blockly.FieldCheckbox(this.inputs_[i] ? "TRUE" : "FALSE"), BORDER_FIELDS[i].toUpperCase())
+                .appendField(names[i])
+        }
+        containerBlock.initSvg();
+        return containerBlock;
+    },
+
+    compose: function (containerBlock) {
+        // Set states
+        for (let i = 0; i < this.inputs_.length; i++) {
+            this.inputs_[i] = (containerBlock.getFieldValue(BORDER_FIELDS[i].toUpperCase()) == "TRUE");
+        }
+        this.updateShape_();
+    },
+
+    updateShape_: function () {
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if ((!this.inputs_[i]) && (this.getInput(BORDER_FIELDS[i].toUpperCase()))) this.removeInput(BORDER_FIELDS[i].toUpperCase());
+        }
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if ((this.inputs_[i]) && (!(this.getInput(BORDER_FIELDS[i].toUpperCase())))) {
+                BaseBlockly.Msg[BORDER_FIELDS[i]] = names[i];
+                this.appendValueInput(BORDER_FIELDS[i].toUpperCase())
+                    .setCheck(BORDER_TYPES[i])
+                    .setAlign(Blockly.ALIGN_RIGHT)
+                    .appendField(names[i]);
+            }
+        }
     }
-    const code = `embed.setAuthor({name: String(${author}), iconURL: String(${profile}), url: String(${url})}); \n`;
-    return code;
+
+};
+
+Blockly.JavaScript[blockName] = function (block) {
+const author = Blockly.JavaScript.valueToCode(block, "AUTHOR", Blockly.JavaScript.ORDER_NONE);
+ const profile = Blockly.JavaScript.valueToCode(block, "PROFILE", Blockly.JavaScript.ORDER_NONE);
+    const url = Blockly.JavaScript.valueToCode(block, "URL", Blockly.JavaScript.ORDER_NONE);
+
+var code = `embed.setAuthor({`
+  
+    if(author) {
+      code += `name: String(${name}),`
+    }
+  if(profile) {
+    code += ` iconUrl: String(${profile}),`
+  }
+  if(url){
+code += `url: String(${url})`
+  }
+  var finalcode = `${code}}); \n`
+    return finalcode;
 };
 
 registerRestrictions(blockName, [
