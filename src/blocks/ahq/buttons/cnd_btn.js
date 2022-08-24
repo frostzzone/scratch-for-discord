02cab29088e536502866b421bf31a3018fbc9cac
AlexCdDg2 <105122938+AlexCdDg2@users.noreply.github.com>
2022-08-23 22:03:30 -0300

    #mutators_forever

--- src/blocks/ahq/buttons/cnd_btn.js
+++ src/blocks/ahq/buttons/cnd_btn.js
@@ -1,5 +1,15 @@
-import * as Blockly from "blockly";
+import * as Blockly from "blockly/core";
+import BaseBlockly from "blockly";
+
+
+
 const blockName = "snd_ahq_button";
+  const BORDER_FIELDS = ["LABEL", "MESSAGE", "BUTTON", "EMBED"];
+const BORDER_TYPES = ["Channel","String","", ""];
+const names = ["on channel", "text", "button", "embed"];
+const amountOfInputs = names.length
+const menuName = 'idk'
+
 const ahqcolor = ['#33cc00', '#33cc00', '#33cc00', '#33cc00'];
 function listsGetRandomItem(list, remove) {
     var x = Math.floor(Math.random() * list.length);
@@ -9,64 +19,110 @@ function listsGetRandomItem(list, remove) {
         return list[x];
     }
 }
-const blockData = {
-    "message0": "send button on channel %1 %2 text %3 %4 button %5 %6 embed %7",
-    "args0": [
-        {
-            "type": "input_value",
-            "name": "Label",
-            "check": ["Channel"]
-        },
-        {
-            "type": "input_dummy"
-        },
-        {
-            "type": "input_value",
-            "name": "button name",
-            "check": "String"
-        },
-        {
-            "type": "input_dummy"
-        },
-        {
-            "type": "input_value",
-            "name": "button val",
-            "check": "String"
-        },
-        {
-            "type": "input_dummy"
-        },
-        {
-            "type": "input_value",
-            "name": "embed val",
-            "check": ""
-        },
 
-    ],
+const blockData = {
+    "message0": "send button",
     "colour": listsGetRandomItem(ahqcolor, false),
     "previousStatement": null,
     "nextStatement": null,
+    "helpUrl": ""
 };
 
+
+Blockly.Blocks[menuName] = {
+    init: function () {
+        this.setColour(listsGetRandomItem(ahqcolor, false));
+        this.setHelpUrl("");
+    }
+};
 Blockly.Blocks[blockName] = {
     init: function () {
         this.jsonInit(blockData);
+        this.setMutator(new Blockly.Mutator([]));
+        this.inputs_ = []
+        for (let i = 0; i < amountOfInputs; i++) {
+            this.inputs_.push(false)
+        }
+    },
+
+
+    mutationToDom: function () {
+        if (!this.inputs_) {
+            return null;
+        }
+        const container = document.createElement("mutation");
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if (this.inputs_[i]) container.setAttribute(BORDER_FIELDS[i], this.inputs_[i])
+        }
+        return container;
+    },
+
+    domToMutation: function (xmlElement) {
+        for (let i = 0; i < this.inputs_.length; i++) {
+            this.inputs_[i] = xmlElement.getAttribute(BORDER_FIELDS[i].toLowerCase()) == "true";
+        }
+        this.updateShape_();
+    },
+
+    decompose: function (workspace) {
+        const containerBlock = workspace.newBlock(menuName);
+        for (let i = 0; i < this.inputs_.length; i++) {
+            BaseBlockly.Msg[BORDER_FIELDS[i]] = names[i];
+            containerBlock.appendDummyInput()
+                .setAlign(Blockly.ALIGN_LEFT)
+                .appendField(new Blockly.FieldCheckbox(this.inputs_[i] ? "TRUE" : "FALSE"), BORDER_FIELDS[i].toUpperCase())
+                .appendField(names[i])
+        }
+        containerBlock.initSvg();
+        return containerBlock;
+    },
+
+    compose: function (containerBlock) {
+        // Set states
+        for (let i = 0; i < this.inputs_.length; i++) {
+            this.inputs_[i] = (containerBlock.getFieldValue(BORDER_FIELDS[i].toUpperCase()) == "TRUE");
+        }
+        this.updateShape_();
+    },
+
+    updateShape_: function () {
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if ((!this.inputs_[i]) && (this.getInput(BORDER_FIELDS[i].toUpperCase()))) this.removeInput(BORDER_FIELDS[i].toUpperCase());
+        }
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if ((this.inputs_[i]) && (!(this.getInput(BORDER_FIELDS[i].toUpperCase())))) {
+                BaseBlockly.Msg[BORDER_FIELDS[i]] = names[i];
+                this.appendValueInput(BORDER_FIELDS[i].toUpperCase())
+                    .setCheck(BORDER_TYPES[i])
+                    .setAlign(Blockly.ALIGN_RIGHT)
+                    .appendField(names[i]);
+            }
+        }
     }
+
 };
+
 Blockly.JavaScript[blockName] = function (block) {
-    let extra = "";
-    const name = Blockly.JavaScript.valueToCode(block, "Label", Blockly.JavaScript.ORDER_NONE);
-    const data = Blockly.JavaScript.valueToCode(block, "button name", Blockly.JavaScript.ORDER_NONE);
-    const embed = Blockly.JavaScript.valueToCode(block, "embed val", Blockly.JavaScript.ORDER_NONE);
-    if (embed) {
+  var extra;
+const label = Blockly.JavaScript.valueToCode(block, "LABEL", Blockly.JavaScript.ORDER_NONE);
+ const message = Blockly.JavaScript.valueToCode(block, "MESSAGE", Blockly.JavaScript.ORDER_NONE);
+    const button = Blockly.JavaScript.valueToCode(block, "BUTTON", Blockly.JavaScript.ORDER_NONE);
+    const embed = Blockly.JavaScript.valueToCode(block, "EMBED", Blockly.JavaScript.ORDER_NONE);
+const statementsThen = Blockly.JavaScript.valueToCode(block, "BUTTON", Blockly.JavaScript.ORDER_NONE);
+    const ahq = statementsThen.replace("'", "").replace("'", "");
+var code = `${label}.send({`
+  if (embed) {
         extra = `${embed}`;
     }
-    const statementsThen = Blockly.JavaScript.valueToCode(block, "button val", Blockly.JavaScript.ORDER_NONE);
-    const ahq = statementsThen.replace("'", "").replace("'", "");
-    const code = `${name}.send({
-        content: String(${data}),
-        components: [new MessageActionRow().addComponents(${ahq})],
-        ${extra.replace("`", "").replace("`", "").replace("'", "").replace("'", "")}
-        });`;
-    return code;
+    if(message) {
+      code += `content: String(${message}),`
+    }
+  if(button) {
+    code += ` components: [new MessageActionRow().addComponents(${ahq})],`
+  }
+  if(extra){
+code += `${extra.replace("`", "").replace("`", "").replace("'", "").replace("'", "")}`
+  }
+  var finalcode = `${code}}); \n`;
+    return finalcode;
 };
